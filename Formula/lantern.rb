a48747c0731fe940c0efe79bf29d6630056df795
Varik Matevosyan <varikmatevosyan@gmail.com>
2024-06-12 11:24:21 +0400

    lantern 0.3.0

--- Formula/lantern.rb
+++ Formula/lantern.rb
@@ -1,9 +1,9 @@
 class Lantern < Formula
   desc "Is a postgres extension that provides blazingly fast vector indexes"
   homepage "https://lantern.dev"
-  url "https://github.com/lanterndata/lantern/releases/download/v0.2.7/lantern-v0.2.7-source.tar.gz"
-  version "0.2.7"
-  sha256 "192b3f330291df6a59a56f44f08c6e83595e57facafeb2b43da398639a3e0c30"
+  url "https://github.com/lanterndata/lantern/releases/download/v0.3.0/lantern-v0.3.0-source.tar.gz"
+  version "0.3.0"
+  sha256 "6025605cd8e8d6a4ca04a76e7574034ef812a8805d847e3d4cbf5f8fc6203c52"
 
   license "MIT"
 
